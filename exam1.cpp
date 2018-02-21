2d9d66bb0eb8cf94a82d075ce5abcf07f9741bb9
Unknown <Chonthichayon@cvc-cha.ac.th>
2018-02-21 12:02:01 +0700

    ข้อ 2

--- exam1.cpp
+++ exam1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
 int main() {
+    cout <<"Welcome to Calculator V1.00">>
 
         return 0;
 }
--- exam2.cpp
+++ exam2.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 int main() {
+
+    int cmd;
+    ofstream writeFile ("std.txt", ofstream::app);
+    ifstream readFile ("std.txt");
+    string line;
+    string name;
+
+    cin >> cmd;
+
+    if (cmd = 1){
+        cin >> name;{
+        if (writeFile.is_open()){
+            writeFile << name << endl;
+            writeFile.close();
+        } else {
+            cout << "Unable to open file";
+        }
+    } if (cmd = 2){
+            if (readFile.is_open()){
+                while (getline(readFile, line)){
+                        cout << line << endl;
+                }
+                readFile.close();
+            }
+
+        } else {
+            return 0;
+        }
+
+
+            return 0;
+
+    }
+
+
+
+
 
         return 0;
 }
